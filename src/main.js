e63bed1842b57dc5143b199441925e125adb6601
Jorge Bárcena <barcenaferruz2022@iessantiagohernandez.com>
2024-12-18 11:29:38 +0100

    18/12

--- src/main.js
+++ src/main.js
@@ -1,50 +1,26 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAXxyYhSXkua2z9LIMJDcHG07HaVdkXx-8",
-  authDomain: "nodogram-1cfcc.firebaseapp.com",
-  projectId: "nodogram-1cfcc",
-  storageBucket: "nodogram-1cfcc.firebasestorage.app",
-  messagingSenderId: "776473842961",
-  appId: "1:776473842961:web:42bb71af130493a11328b0",
-  measurementId: "G-DMTS85M6KK"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
+import { addEntry, getData } from "./db";
 
 
 addEventListener('submit', () => {
   addEntry();
 });
 
-function addEntry() {
-  try {
-    addDoc(collection(db,'entry'), {
-      text: document.querySelector('#input-text').value,
-      user: document.querySelector('#input-name').value
-    });
-    console.log("añadido");
-  }
-  catch(e) {
-    console.log('Error al insertar en la base de datos...');
-  }
-}
+/*setInterval(() => {
+  if (document.querySelector("#list") !== null) {
+    document.querySelector("#list").remove();
 
-async function getData() {
-  const data = await getDocs(collection(db,'entry'));
-  data.forEach(element => {
-    let li = document.createElement('LI');
-    let {user, text} = element.data();
-    li.innerHTML = `Usuario: ${user}, Texto: ${text}` ;
-  
-    document.querySelector("#list").appendChild(li);
-  });
-}
+  }
+  const list = document.createElement("UL");
+  list.setAttribute("id", "list");
+  document.body.appendChild(list);*/
+  getData();
+//}, 1000);
 
-getData();
+
+
+
+
+
 
 
 
